43a72a1027b0ae5605eb8c41f1f099570813ce55
Dominik <domkaukinen@gmail.com>
2017-07-22 19:08:40 -0400

    aded conversation params, removed bucketing

--- util/hyperparamutils.py
+++ util/hyperparamutils.py
@@ -13,7 +13,7 @@ def restore_hyper_params(file_path):
     with open(file_path, 'rb') as handle:
         return pickle.load(handle)
 
-def save_hyper_params(checkpoint_dir, FLAGS, buckets, conversation_limits):
+def save_hyper_params(checkpoint_dir, FLAGS):
     '''
     Serialize hyper parameters fopr checkpoint restoration later
     Inputs:
@@ -29,9 +29,9 @@ def save_hyper_params(checkpoint_dir, FLAGS, buckets, conversation_limits):
            "num_layers" : FLAGS.num_layers,
            "learning_rate" : FLAGS.learning_rate,
            "lr_decay_factor" : FLAGS.lr_decay_factor,
-           "max_source_length": conversation_limits[0],
-           "max_target_length":conversation_limits[1],
-           "conversation_history": conversation_limits[2]
+           "max_source_length": FLAGS.max_source_length,
+           "max_target_length": FLAGS.max_target_length,
+           "convo_limits": FLAGS.convo_limits
            }
 
     path = os.path.join(checkpoint_dir, "hyperparams.p")
